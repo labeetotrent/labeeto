b58a369a6b4a0c22eb943deebdad8afdc44e10e0
Andrey Zhilin <andzhilin@gmail.com>
2014-11-13 00:47:42 +0300

    Some feed page amendments

--- hoc/protected/modules/site/views/layouts/feed.php
+++ hoc/protected/modules/site/views/layouts/feed.php
@@ -22,6 +22,7 @@
     <link type="text/css" href="<?php echo Yii::app()->themeManager->baseUrl; ?>/blue/jplayer.blue.monday.css" rel="stylesheet">
     <link type="text/css" href="<?php echo Yii::app()->themeManager->baseUrl; ?>/css/smoothDivScroll.css" rel="stylesheet">
     <link type="text/css" href="<?php echo Yii::app()->themeManager->baseUrl; ?>/css/slashman.css" rel="stylesheet">
+    <link type="text/css" href="<?php echo Yii::app()->themeManager->baseUrl; ?>/css/slashman_feed.css" rel="stylesheet">
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
