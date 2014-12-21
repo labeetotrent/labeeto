f58f47518223b6a8e38299b85c237de529ffa5ca
Andrey Zhilin <andzhilin@gmail.com>
2014-12-21 03:26:39 +0300

    New messages

--- hoc/protected/modules/site/controllers/UserController.php
+++ hoc/protected/modules/site/controllers/UserController.php
@@ -77,7 +77,7 @@ class UserController extends SiteBaseController {
         }
         $userOff = User::model()->findByPk( Yii::app()->user->id );
         $userOff->is_online = User::USER_OFFLINE;
-        echo $userOff->birthdate; die();
+        echo $userOff->birthday; die();
         $userOff->save(array('is_online'));
 
         Yii::app()->user->logout(true);
