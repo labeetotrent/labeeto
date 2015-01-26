31461e5c52f64d1e92b7d75ee81643596673d2b7
Andrey Zhilin <andzhilin@gmail.com>
2015-01-26 19:21:31 +0300

    Facebook integration

--- hoc/protected/components/FacebookIdentity.php
+++ hoc/protected/components/FacebookIdentity.php
@@ -20,33 +20,37 @@ class FacebookIdentity extends CUserIdentity
     public function authenticate()
     {
         $record = User::model()->findByAttributes(array('facebook_id' => $this->username));
-
-        if($record->status ==1) {
-            $this->errorCode=self::ERROR_BLOCKED;
-            $this->errorMessage = Yii::t('members', 'Sorry, But the account is blocked.');
-        }
-        else {
-            $this->_id = $record->id;
-
-            $auth=Yii::app()->authManager;
-            if(!$auth->isAssigned($record->role,$this->_id))  {
-                if($auth->assign($record->role,$this->_id))
-                {
-                    Yii::app()->authManager->save();
-                }
+        if($record)
+        {
+            if($record->status ==1) {
+                $this->errorCode=self::ERROR_BLOCKED;
+                $this->errorMessage = Yii::t('members', 'Sorry, But the account is blocked.');
             }
-            // We add username to the state
-            //$this->setState('fullname', $record->firstname." ".$record->lastname);
-            $this->setState('username', $record->username);
-            $this->setState('email', $record->email);
-            $this->setState('role', $record->role);
-            $this->setState('facebook_id', $record->facebook_id);
-            $this->setState('facebook_token', $record->facebook_token);
-            $this->errorCode = self::ERROR_NONE;
-            $record->last_logged = date('Y-m-d H:i:s');
-            $record->is_online   = User::USER_ONLINE;
-            $record->save();
+            else {
+                $this->_id = $record->id;
+
+                $auth=Yii::app()->authManager;
+                if(!$auth->isAssigned($record->role,$this->_id))  {
+                    if($auth->assign($record->role,$this->_id))
+                    {
+                        Yii::app()->authManager->save();
+                    }
+                }
+                // We add username to the state
+                //$this->setState('fullname', $record->firstname." ".$record->lastname);
+                $this->setState('username', $record->username);
+                $this->setState('email', $record->email);
+                $this->setState('role', $record->role);
+                $this->setState('facebook_id', $record->facebook_id);
+                $this->setState('facebook_token', $record->facebook_token);
+                $this->errorCode = self::ERROR_NONE;
+                $record->last_logged = date('Y-m-d H:i:s');
+                $record->is_online   = User::USER_ONLINE;
+                $record->save();
+            }
         }
+        else
+            $this->errorCode = self::ERROR_UNKNOWN_IDENTITY;
 
         return !$this->errorCode;
     }
