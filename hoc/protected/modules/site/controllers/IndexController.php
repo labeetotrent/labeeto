42f3478a1dc201bc17064ab0d205be7a129891e3
Andrey Zhilin <andzhilin@gmail.com>
2015-01-26 08:51:09 +0300

    Facebook integration

--- hoc/protected/modules/site/controllers/IndexController.php
+++ hoc/protected/modules/site/controllers/IndexController.php
@@ -50,14 +50,14 @@ class IndexController extends SiteBaseController {
             try {
                 $facebook = new Facebook($session);
                 echo '1';
-                if($facebook->register()) // If user is new
+                /*if($facebook->register()) // If user is new
                 {
                     $this->redirect(array('/site/my_feed'));
                 }
                 else //If user was already registered
                 {
                     $this->redirect(array('/site/my_feed'));
-                }
+                }*/
 
                 $user_profile = (new FacebookRequest(
                     $session, 'GET', '/me'
