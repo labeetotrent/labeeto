0a37ab7f98eb61cd41078d74f068cdcf0ddaab7e
Andrey Zhilin <andzhilin@gmail.com>
2015-01-26 07:01:14 +0300

    Facebook integration

--- hoc/protected/modules/site/controllers/IndexController.php
+++ hoc/protected/modules/site/controllers/IndexController.php
@@ -39,13 +39,6 @@ class IndexController extends SiteBaseController {
             // When validation fails or other local issues
             CVarDumper::dump($ex, 100, true);
         }
-        if (isset($session)) {
-            echo '3';
-            CVarDumper::dump($session, 100, true);
-            // Logged in.
-        }
-        else
-            var_dump($session);
 
         echo '4';
     }
