77f06e659f2c73c9191ed45b3500ec188acc7500
Andrey Zhilin <andzhilin@gmail.com>
2015-01-26 06:37:26 +0300

    Facebook integration

--- hoc/protected/modules/site/controllers/IndexController.php
+++ hoc/protected/modules/site/controllers/IndexController.php
@@ -27,6 +27,7 @@ class IndexController extends SiteBaseController {
     {
         echo '0';
         $helper = new FacebookRedirectLoginHelper();
+        var_dump($helper);
         try {
             $session = $helper->getSessionFromRedirect();
         } catch(FacebookRequestException $ex) {
