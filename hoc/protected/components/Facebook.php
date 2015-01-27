f5b423d63b20718c894aabe6e937f38b13f80ef1
Andrey Zhilin <andzhilin@gmail.com>
2015-01-27 07:41:07 +0300

    Profile page view

--- hoc/protected/components/Facebook.php
+++ hoc/protected/components/Facebook.php
@@ -78,7 +78,7 @@ class Facebook {
             }
             else
             {
-                var_dump($user_info->getProperty('bio'));
+                var_dump($user_info);
                 die();
                 $avatar = $this->getAvatar();
                 $dbUser = new User();
