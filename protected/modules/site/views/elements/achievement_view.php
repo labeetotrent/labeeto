35162329e8d9fb29cf2e135f02953fd1eff24f7d
trentbrookes@gmail.com <trentbrookes@gmail.com>
2015-04-01 05:51:04 +0000

    feed fixes

--- protected/modules/site/views/elements/achievement_view.php
+++ protected/modules/site/views/elements/achievement_view.php
@@ -25,7 +25,7 @@ if($user){?>
                     <?php } ?>
                 </h2>
                 <h2>
-                    <span class="time-location">18 F, CA,</span> 
+                    <span class="time-location"><?= date("Y") - date('Y', strtotime($user->birthday));  ?> <?php  if($user->gender == 1) echo "F"; else echo 'M'; ?>- <?= $user->address; ?></span> 
                     <span class="dot-icon"> <?php echo Achievements::time_elapsed_string($data->created); ?></span>
                 </h2>
                 <!--<a class="message" data-id="<?php //echo $user->id; ?>" data-toggle="modal" data-target="#SendaMessage">Send a Message</a>
